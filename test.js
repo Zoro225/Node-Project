96d83d3fb3fa2afe97ca936a2db949890cc993c6
Amitjadhao <amitjadhao26@gmail.com>
2026-02-01 10:16:37 +0000

    some

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-const assert = require('assert');
-describe('Jenkins Test', () => {
-  it('passes', () => assert.ok(true));
-});
+              var request = require('supertest');
+              var app = require('../index.js');
+              
+              describe('GET /', function () {
+                after(function () {
+                  process.exit(0); // Ensure Mocha exits after tests
+                });
+              
+                it('respond with 404 page not found', function (done) {
+                  request(app)
+                    .get('/nonexistentpage')
+                    .expect(404, done);
+                });
+              });
